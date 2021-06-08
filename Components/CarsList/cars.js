84387e7db12e196918c80954f30eb7139d203235
Shuvo Goswami <avitalukder20158@gmail.com>
2021-06-08 20:53:36 +0600

    car added

---
+++ Components/CarsList/cars.js
@@ -0,0 +1,17 @@
+export default [{
+    name: 'Mahindra Bolero',
+    tagline: 'Starting at $50000',
+    image: require('../../assets/images/car1.jpg'),
+}, {
+    name: 'Land Rover Range Rover',
+    tagline: 'Starting at $60000',
+    image: require('../../assets/images/car2.jpg'),
+}, {
+    name: 'Hyundai i20',
+    tagline: 'Starting at $70000',
+    image: require('../../assets/images/car3.jpg'),
+}, {
+    name: 'Kia Sonet',
+    tagline: 'Starting at $80000',
+    image: require('../../assets/images/car4.jpg'),
+}];
